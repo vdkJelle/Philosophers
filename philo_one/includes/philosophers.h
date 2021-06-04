b9d65c88ec4930f764ac93314e517f099266b2a3
vdkJelle <vdkjelle@gmail.com>
2021-06-04 20:11:25 +0200

    work on philo from home

--- philo_one/includes/philosophers.h
+++ philo_one/includes/philosophers.h
@@ -6,12 +6,13 @@
 /*   By: jelvan-d <jelvan-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/24 11:29:45 by jelvan-d      #+#    #+#                 */
-/*   Updated: 2021/05/24 17:32:15 by jelvan-d      ########   odam.nl         */
+/*   Updated: 2021/06/01 11:28:12 by jelvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS
 # define PHILOSOPHERS
+# define YOURMOM -1
 
 # include <pthread.h>
 # include <stdio.h>
@@ -28,6 +29,18 @@ typedef struct	s_data
 	ssize_t	ms_to_sleep;
 	ssize_t	iter;
 }				t_data;
+
+typedef struct	s_philo
+{
+	pthread_t	philosopher;
+	ssize_t		i;
+	ssize_t		ms_to_die;
+	ssize_t		ms_to_eat;
+	ssize_t		ms_to_sleep;
+	ssize_t		iter;
+	ssize_t		prev_time_ms;
+	ssize_t		time_ms;
+}				t_philo;
 
 /*
 *************************************MAIN.C*************************************
--- philo_one/src/philosophers.c
+++ philo_one/src/philosophers.c
@@ -6,44 +6,62 @@
 /*   By: jelvan-d <jelvan-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/24 16:26:13 by jelvan-d      #+#    #+#                 */
-/*   Updated: 2021/05/24 17:43:50 by jelvan-d      ########   odam.nl         */
+/*   Updated: 2021/06/01 11:27:57 by jelvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-static int	free_and_return(pthread_t *philosophers, int err)
+static int	free_and_return(t_philo **philo, int err)
 {
-	free(philosophers);
-	// free(*(err));
+	free(*philo);
 	return (err);
 }
 
-static void	*philo_action(void *data)
+static void	initialise_philo(t_data *data, t_philo *philo)
 {
-	(void)data;
-	printf("hello!\n");
+	philo->ms_to_die = data->ms_to_die;
+	philo->ms_to_eat = data->ms_to_eat;
+	philo->ms_to_sleep = data->ms_to_sleep;
+	philo->iter = data->iter;
+	philo->prev_time_ms = 0;
+	philo->time_ms = 0;
+}
+
+static void	*philo_action(void *ptr)
+{
+	pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+	t_philo *philo;
+
+	philo = (t_philo*)ptr;
+	pthread_mutex_lock(&lock);
+	printf("Philo[%ld] reporting in!\n", philo->i);
+	pthread_mutex_unlock(&lock);
 	return NULL;
 }
 
 int			philosophers(t_data *data)
 {
-	pthread_t	*philosophers;
-	int			i;
+	t_philo	*philo;
+	ssize_t	i;
 
-	philosophers = malloc(sizeof(pthread_t) * data->nb_philo);
+	philo = malloc(sizeof(t_philo) * data->nb_philo);
+	if (!philo)
+		return (YOURMOM);
 	i = 0;
 	while (i < data->nb_philo)
 	{
-		pthread_create(philosophers + i, NULL, philo_action, data);
+		pthread_create(&philo[i].philosopher, NULL, philo_action, &philo[i]);
+		initialise_philo(data, &philo[i]);
+		philo[i].i = i + 1;
 		i++;
 	}
 	i = 0;
 	while (i < data->nb_philo)
 	{
-		if (pthread_join(philosophers[i], NULL) != 0)
-			return (free_and_return(philosophers, 1));
+		if (pthread_join(philo[i].philosopher, NULL) != 0)
+			return (free_and_return(&philo, 1));
 		i++;
 	}
-	return (free_and_return(philosophers, 0));
+	return (free_and_return(&philo, 0));
 }
